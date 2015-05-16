ea321e7e9d412eb56a67328272365db29d5733ad
JaredShay <jared.shay@gmail.com>
2015-05-16 10:29:14 -0500

    fix requirements and aus_post main spec

---
+++ aus_post.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |gem|
+  gem.authors       = ["JaredShay"]
+  gem.email         = ["jared.shay@gmail.com"]
+  gem.description   = %q{Wrapper for Australia Post's developer API}
+  gem.summary       = %q{Wrapper for Australia Post's developer API}
+  gem.homepage      = "https://github.com/jaredshay/aus_post"
+
+  gem.files         = `git ls-files`.split("\n")
+  gem.test_files    = `git ls-files -- spec/*`.split("\n")
+  gem.name          = "aus_post"
+  gem.require_paths = ["lib"]
+  gem.version       = '1.0.0'
+end
--- lib/aus_post.rb
+++ lib/aus_post.rb
@@ -1,21 +1,23 @@
+Dir[(File.dirname(__dir__)) + "/lib/**/*.rb"].each { |f| require f }
+
 class AusPost
   def initialize(config)
     @config = config
   end
 
   def postage_parcel_domestic_service(params)
-    Postage::Parcel::Domestic::Service.new(params, config).execute
+    Postage::Parcel::Domestic::Service.new(params, @config).execute
   end
 
   def postage_parcel_domestic_calculate(params)
-    Postage::Parcel::Domestic::Calculate.new(params, config).execute
+    Postage::Parcel::Domestic::Calculate.new(params, @config).execute
   end
 
   def postage_parcel_international_service(params)
-    Postage::Parcel::International::Service.new(params, config).execute
+    Postage::Parcel::International::Service.new(params, @config).execute
   end
 
   def postage_parcel_international_calculate(params)
-    Postage::Parcel::International::Calculate.new(params, config).execute
+    Postage::Parcel::International::Calculate.new(params, @config).execute
   end
 end
