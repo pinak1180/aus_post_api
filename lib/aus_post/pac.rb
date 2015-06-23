fef1dfa802a2c486ffc5ce1e4918ac1eeb0ca0d4
JaredShay <jared.shay@gmail.com>
2015-06-23 16:12:46 +1000

    set default params to an empty hash. Some endpoints don't require params, better off just setting them all to empty

--- lib/aus_post/pac.rb
+++ lib/aus_post/pac.rb
@@ -1,38 +1,38 @@
 class AusPost
   module PAC
-    def pac_domestic_postcode_search(params)
+    def pac_domestic_postcode_search(params = {})
       AusPost::PAC::DomesticPostcodeSearch.new(params, @config).execute
     end
 
-    def pac_country(params)
+    def pac_country(params = {})
       AusPost::PAC::Country.new(params, @config).execute
     end
 
-    def pac_domestic_letter_thickness(params)
+    def pac_domestic_letter_thickness(params = {})
       AusPost::PAC::DomesticLetterThickness.new(params, @config).execute
     end
 
-    def pac_domestic_letter_weight(params)
+    def pac_domestic_letter_weight(params = {})
       AusPost::PAC::DomesticLetterWeight.new(params, @config).execute
     end
 
-    def pac_domestic_letter_size(params)
+    def pac_domestic_letter_size(params = {})
       AusPost::PAC::DomesticLetterSize.new(params, @config).execute
     end
 
-    def pac_postage_parcel_domestic_service(params)
+    def pac_postage_parcel_domestic_service(params = {})
       AusPost::PAC::PostageParcelDomesticService.new(params, @config).execute
     end
 
-    def pac_postage_parcel_domestic_calculate(params)
+    def pac_postage_parcel_domestic_calculate(params = {})
       AusPost::PAC::PostageParcelDomesticCalculate.new(params, @config).execute
     end
 
-    def pac_postage_parcel_international_service(params)
+    def pac_postage_parcel_international_service(params = {})
       AusPost::PAC::PostageParcelInternationalService.new(params, @config).execute
     end
 
-    def pac_postage_parcel_international_calculate(params)
+    def pac_postage_parcel_international_calculate(params = {})
       AusPost::PAC::PostageParcelInternationalCalculate.new(params, @config).execute
     end
   end
